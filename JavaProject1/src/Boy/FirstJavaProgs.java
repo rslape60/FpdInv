4c6c2afc452749ddaa0133e84e1dccacdadb1094
rslape60 <54010135+rslape60@users.noreply.github.com>
2019-08-25 11:58:16 +0800

    Initial checkin of JavaProject ni Boy

---
+++ JavaProject1/src/Boy/FirstJavaProgs.java
@@ -0,0 +1,19 @@
+package Boy;
+
+public class FirstJavaProgs {
+	public static void main(String args[])
+	{
+		//code
+		
+		int x =2;
+		int y=7;
+		int r= x+y;
+		String h="FUCK";
+		
+		System.out.println("Boy Lape");
+		System.out.println("Pogi");
+		System.out.println(r);
+
+		System.out.println(x=x++);
+	}
+}
